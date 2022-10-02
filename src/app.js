9a6d5cfec9b279e696135b44c774a8fc827163d2
iryna-sirotina <113804413+iryna-sirotina@users.noreply.github.com>
2022-10-02 18:39:17 +0300

    Edited JS template

--- src/app.js
+++ src/app.js
@@ -40,22 +40,26 @@ function displayForecast(response) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
-        `<div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        `<div class="col-sm-2">
+          <div class="card">
+            <div class="card-body">
+        <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
           src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
-          width="42"
+          width="60"
         />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
+        <div class="forecast-temperatures">
+          <span class="forecast-temperature-max"> ${Math.round(
             forecastDay.temp.max
-          )}° / </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
+          )}°</span> / 
+          <span class="forecast-temperature-min"> ${Math.round(
             forecastDay.temp.min
           )}° </span>
+        </div>
+        </div>
         </div>
       </div>
   `;
